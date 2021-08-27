235c540032fb344af57f760e487ddb188021e9c6
warrior14 <ironaddicts14@gmail.com>
2021-08-27 09:30:03 -0500

    added a Robe.cs file and added public mutable properties

---
+++ Robe.cs
@@ -0,0 +1,13 @@
+
+using System.Collections.Generic;
+
+
+namespace Quest 
+{
+    public class Robe
+    {
+        // Mutable properties 
+        public List<string> Colors = new List<string>();
+        public int RobeLength { get; set; }
+    }
+}
